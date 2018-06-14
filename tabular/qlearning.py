f9c61f2b0396dba3aea5480ce924a66c9c510702
Christopher  Chong <tau.chong14@imperial.ac.uk>
2018-06-14 22:42:26 +0100

    added no slippery version

--- tabular/qlearning.py
+++ tabular/qlearning.py
@@ -1,6 +1,16 @@
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
+from gym.envs.registration import register
+
+register(
+    id='FrozenLakeNotSlippery-v0',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name' : '4x4', 'is_slippery': False},
+    max_episode_steps=100,
+    reward_threshold=0.78, # optimum = .8196
+)
+
 
 def randomSampling(env,num_episode,game_step):
     
@@ -12,6 +22,7 @@ def randomSampling(env,num_episode,game_step):
             action = env.action_space.sample()
             new_state, reward, done, info = env.step(action)
             count += 1
+            env.render()
             if(done == True):
                 break
         goals.append(reward)
@@ -56,7 +67,7 @@ def qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_nois
     print("Final Q Table  =",Q)
     print("No. of plays under 100 game steps = ",counter)
     env.render()
-    return goals
+    return goals, Q
 
 
 def evalEpisode(score,num_episode,title,save,folder):
@@ -77,59 +88,90 @@ def writeResult(filename,folder,params,run):
     
     with open("/vol/bitbucket/ttc14/thesis/result/{0}/{1}.txt".format(folder,filename),"a+") as f:
         if(run==0):
-            f.write("no_of_episodes gamma learning_rate discount_noise punish ith_run avg_score\n")
+            f.write("no_of_episodes gamma learning_rate discount_noise punish ith_run avg_score actual_avg\n")
         for item in params:
             f.write(str(item))
             f.write(" ")
         f.write("\n")
 
+
+def playGame(Q,env,num_episode,game_step):
+
+    goals = []
+    for episode in range(num_episode):
+        state = env.reset()
+        count = 0
+        while count <= game_step:
+            action = np.argmax(Q[state,:])
+            new_state, reward, done, info = env.step(action)
+            count+=1
+            state = new_state
+            if(done == True):
+                break
+        goals.append(reward)
+    return goals
     
+        
 
 def main():
-    game = "FrozenLake-v0"
+    game = "FrozenLakeNotSlippery-v0"
     env = gym.make(game)
 
     ###### Random Action Sampling ########
-    num_epi = 10000
+    '''
+    num_epi = 1
     game_cnt = 100
-    run_cnt = 10
-
+    run_cnt = 1
+    
     for i in range(run_cnt):
         rewards = randomSampling(env,num_epi,game_cnt)
         avg_rewards = sum(rewards)/num_epi
         print(avg_rewards)
-
+    '''
     ############ Q-Learning ###############
     
-    num_episode = 1000
+    num_episode = 180
     game_step = 100
     gamma  = 0.9
     learning_rate = 0.8
     discount_noise = True                   # False to not use any Gaussian noise on action selection
-    diminishing_weight = False              # False to not use the discounted weight for noise in late episodes
+    diminishing_weight = True              # False to not use the discounted weight for noise in late episodes
     punish = False                          # True to set a punishment for stepping into the hole in FrozenLake-v0
     punish_val = -1
 
     run = 5                                 # Number of runs to train the agent 
     save = False                            # True to save the picture generated from evalEpisode()
-    folder = "frozen_lake"
+    folder = "frozen_lake_not_slippery"
     
     for i in range(run):
-        goals = qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val,diminishing_weight)
-        avg_score = sum(goals)/num_episode
-
-        filename = "Tabular_QLearning_Result_of_{0}_{1}_episodes".format(game,num_episode) 
-        params = [num_episode,gamma,learning_rate,discount_noise,punish,i,avg_score]
-        writeResult(filename,folder,params,i)
         
+        (goals, Q) = qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val,diminishing_weight)
+        avg_score = sum(goals)/num_episode
         print("Average score per episode:", avg_score)
 
-    '''    
+        ############ Using Final Q Table to Play Games without further Update ##################
+        # '''
+        actual_goals = playGame(Q,env,num_episode,game_step)
+        actual_avg = sum(actual_goals)/num_episode
+        print("Average actual score per episode:", actual_avg)
+        # '''
+
+        ############## Store the Result ###############
+        
+        filename = "Tabular_QLearning_Result_of_{0}_{1}_episodes".format(game,num_episode) 
+        params = [num_episode,gamma,learning_rate,discount_noise,punish,i,avg_score,actual_avg]
+        
+        # '''
+        writeResult(filename,folder,params,i)
+        # '''
+        
+        ############# Plot the Change of Goal against Episode ####################
+        '''    
         title = "{0}th_Tabular_QLearning_Result_of_{1}_{2}_episodes".format(i,game,num_episode)
         evalEpisode(goals,num_episode,title,save,folder)
         
-    '''
-
+        '''
+        
         
 if __name__ == "__main__":
     main()
