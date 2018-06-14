af6e7425243c00eb87ac9cadfd880263aee13f08
Christopher  Chong <tau.chong14@imperial.ac.uk>
2018-06-14 21:31:38 +0100

    added diminishing noise control & random sampling

--- tabular/qlearning.py
+++ tabular/qlearning.py
@@ -2,29 +2,23 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
-def randomSampling(env):
+def randomSampling(env,num_episode,game_step):
     
-    count  = 0
-    reward = 0
-    while reward < 1:
+    goals = []
+    for episode in range(num_episode):
         env.reset()
-        done = False
-        while done != True:
+        count  = 0
+        while count <= game_step:
             action = env.action_space.sample()
-            state, reward, done, info = env.step(action)
-            # env.render()
-        # if(reward < 1):
-        #     print("**************")
-        #     print("Game over boi!")
-        #     print("**************")
-        count += 1
-    # print("**************")
-    # print("You win at ",count," boi!")
-    # print("**************")
-    print(count)
+            new_state, reward, done, info = env.step(action)
+            count += 1
+            if(done == True):
+                break
+        goals.append(reward)
+    return goals
     
 
-def qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val):
+def qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val,diminishing_weight):
     
     Q = np.zeros([env.observation_space.n,env.action_space.n])
     goals = []
@@ -35,10 +29,13 @@ def qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_nois
         count = 0
         
         while count <= game_step:
-            if discount_noise == True:
+            if discount_noise == True and diminishing_weight == True:
                 action = np.argmax(Q[state,:]+np.random.randn(1,env.action_space.n)*(1/(episode+1)))
+            elif discount_noise == True and diminishing_weight == False:
+                action = np.argmax(Q[state,:]+np.random.randn(1,env.action_space.n))
             else:
                 action = np.argmax(Q[state,:])
+                
             new_state, reward, done, info = env.step(action)
             if (punish == True) and (done == True) and (reward == 0):
                 reward = punish_val
@@ -55,6 +52,7 @@ def qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_nois
         if(acc_reward==punish_val):
             acc_reward = 0
         goals.append(acc_reward)
+        
     print("Final Q Table  =",Q)
     print("No. of plays under 100 game steps = ",counter)
     env.render()
@@ -84,37 +82,54 @@ def writeResult(filename,folder,params,run):
             f.write(str(item))
             f.write(" ")
         f.write("\n")
-        
 
     
 
 def main():
     game = "FrozenLake-v0"
     env = gym.make(game)
-    # randomSampling(env)
+
+    ###### Random Action Sampling ########
+    num_epi = 10000
+    game_cnt = 100
+    run_cnt = 10
+
+    for i in range(run_cnt):
+        rewards = randomSampling(env,num_epi,game_cnt)
+        avg_rewards = sum(rewards)/num_epi
+        print(avg_rewards)
 
     ############ Q-Learning ###############
-    num_episode = 100000
+    
+    num_episode = 1000
     game_step = 100
     gamma  = 0.9
     learning_rate = 0.8
-    discount_noise = True
-    punish = False
+    discount_noise = True                   # False to not use any Gaussian noise on action selection
+    diminishing_weight = False              # False to not use the discounted weight for noise in late episodes
+    punish = False                          # True to set a punishment for stepping into the hole in FrozenLake-v0
     punish_val = -1
 
-    run = 5
-    save = False
+    run = 5                                 # Number of runs to train the agent 
+    save = False                            # True to save the picture generated from evalEpisode()
     folder = "frozen_lake"
     
     for i in range(run):
-        goals = qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val)
-        title = "{0}th_Tabular_QLearning_Result_of_{1}_{2}_episodes".format(i,game,num_episode)
-        # evalEpisode(goals,num_episode,title,save,folder)
-        filename = "Tabular_QLearning_Result_of_{0}_{1}_episodes".format(game,num_episode) 
+        goals = qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val,diminishing_weight)
         avg_score = sum(goals)/num_episode
+
+        filename = "Tabular_QLearning_Result_of_{0}_{1}_episodes".format(game,num_episode) 
         params = [num_episode,gamma,learning_rate,discount_noise,punish,i,avg_score]
         writeResult(filename,folder,params,i)
+        
         print("Average score per episode:", avg_score)
-    
+
+    '''    
+        title = "{0}th_Tabular_QLearning_Result_of_{1}_{2}_episodes".format(i,game,num_episode)
+        evalEpisode(goals,num_episode,title,save,folder)
+        
+    '''
+
+        
 if __name__ == "__main__":
     main()
