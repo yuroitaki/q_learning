b25ab2e6beeefe7bab6199b6b686e1b71c641915
Christopher  Chong <tau.chong14@imperial.ac.uk>
2018-06-14 17:48:17 +0100

    added graph and writing function

--- tabular/qlearning.py
+++ tabular/qlearning.py
@@ -61,7 +61,7 @@ def qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_nois
     return goals
 
 
-def evalEpisode(score,num_episode,title,save):
+def evalEpisode(score,num_episode,title,save,folder):
     
     fig = plt.figure(figsize=(32,16))
     x = [i+1 for i in range(num_episode)]
@@ -71,9 +71,22 @@ def evalEpisode(score,num_episode,title,save):
     plt.ylabel("Average Score")
     plt.show()
     if save == True:
-        fig.savefig("fig/{0}.png".format(title),dpi=100)
+        fig.savefig("/vol/bitbucket/ttc14/thesis/fig/{0}/{1}.png".format(folder,title),dpi=100)
     plt.close()
 
+    
+def writeResult(filename,folder,params,run):
+    
+    with open("/vol/bitbucket/ttc14/thesis/result/{0}/{1}.txt".format(folder,filename),"a+") as f:
+        if(run==0):
+            f.write("no_of_episodes gamma learning_rate discount_noise punish ith_run avg_score\n")
+        for item in params:
+            f.write(str(item))
+            f.write(" ")
+        f.write("\n")
+        
+
+    
 
 def main():
     game = "FrozenLake-v0"
@@ -81,21 +94,26 @@ def main():
     # randomSampling(env)
 
     ############ Q-Learning ###############
-    num_episode = 100
+    num_episode = 100000
     game_step = 100
     gamma  = 0.9
     learning_rate = 0.8
     discount_noise = True
     punish = False
     punish_val = -1
-    run = 10
+
+    run = 5
+    save = False
+    folder = "frozen_lake"
     
-    for i in range(3):
+    for i in range(run):
         goals = qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val)
-        title = "{0}th_Tabular_QLearning_Result_of_{1}".format(i,game)
-        save = True
-        evalEpisode(goals,num_episode,title,save)
+        title = "{0}th_Tabular_QLearning_Result_of_{1}_{2}_episodes".format(i,game,num_episode)
+        # evalEpisode(goals,num_episode,title,save,folder)
+        filename = "Tabular_QLearning_Result_of_{0}_{1}_episodes".format(game,num_episode) 
         avg_score = sum(goals)/num_episode
+        params = [num_episode,gamma,learning_rate,discount_noise,punish,i,avg_score]
+        writeResult(filename,folder,params,i)
         print("Average score per episode:", avg_score)
     
 if __name__ == "__main__":
