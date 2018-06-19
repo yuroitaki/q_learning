aa2ab57156f157b9bc1a7934c14636d8d1dc9e55
Christopher  Chong <tau.chong14@imperial.ac.uk>
2018-06-19 22:32:55 +0100

    added epsilon greedy and moving average

--- tabular/qlearning.py
+++ tabular/qlearning.py
@@ -29,30 +29,39 @@ def randomSampling(env,num_episode,game_step):
     return goals
     
 
-def qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val,diminishing_weight):
+def qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val,diminishing_weight,max_epsilon,min_epsilon):
     
     Q = np.zeros([env.observation_space.n,env.action_space.n])
     goals = []
     counter  = 0
+    action_count = []
     for episode in range(num_episode):
         state = env.reset()
         acc_reward = 0
         count = 0
-        
+        act_count = 0
         while count <= game_step:
+            
+            ############### Exploration Strategy ########################
             if discount_noise == True and diminishing_weight == True:
                 action = np.argmax(Q[state,:]+np.random.randn(1,env.action_space.n)*(1/(episode+1)))
             elif discount_noise == True and diminishing_weight == False:
                 action = np.argmax(Q[state,:]+np.random.randn(1,env.action_space.n))
             else:
-                action = np.argmax(Q[state,:])
+                action = epsilonGreedy(Q,state,num_episode,episode,max_epsilon,min_epsilon,env)
+
+            if(action == np.argmax(Q[state,:])):
+               act_count+=1
+                
+            #############################################################
                 
             new_state, reward, done, info = env.step(action)
             if (punish == True) and (done == True) and (reward == 0):
                 reward = punish_val
 
             optimal_Q = np.max(Q[new_state,:])
-            Q[state,action] = Q[state,action] + learning_rate*(reward + gamma*(optimal_Q) - Q[state,action])
+            td_delta = reward + gamma*(optimal_Q) - Q[state,action]
+            Q[state,action] += learning_rate*(td_delta)
             acc_reward += reward
             state = new_state
             count+=1
@@ -63,25 +72,71 @@ def qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_nois
         if(acc_reward==punish_val):
             acc_reward = 0
         goals.append(acc_reward)
+        action_count.append(act_count)
         
     print("Final Q Table  =",Q)
+    print("Final TD Delta = ",td_delta)
     print("No. of plays under 100 game steps = ",counter)
+    print("Greedy action count =",sum(action_count)/len(action_count),action_count)
     env.render()
     return goals, Q
 
 
-def evalEpisode(score,num_episode,title,save,folder):
+def epsilonGreedy(Q,state,num_episode,episode,max_epsilon,min_epsilon,env):
+
+    ########## Linear Decay ###############
+    '''
+    use_epsilon = -(episode/num_episode) + max_epsilon
+    if(use_epsilon < min_epsilon):
+        use_epsilon = min_epsilon
+    ''' 
+    ########## Exponential Decay ##########
+    # '''
+    use_epsilon = max_epsilon * (1/(episode+1))
+    # '''
     
+    rand_num = np.random.uniform(0,1) * (1/(episode+1))
+    if(use_epsilon > rand_num):
+        action = np.random.randint(0,env.action_space.n)
+    else:
+        action = np.argmax(Q[state,:])
+        
+    return action
+
+
+def evalEpisode(score,num_episode,episode_window,title,save,folder):
+    
+    y = calcMovingAverage(score,episode_window)
+    x = [i for i in range(episode_window-1,num_episode)]
     fig = plt.figure(figsize=(32,16))
-    x = [i+1 for i in range(num_episode)]
-    plt.scatter(x,score,marker='x')
+    plt.scatter(x,y,marker='x')
     plt.title(title,fontweight='bold')
     plt.xlabel("Episode No.")
-    plt.ylabel("Average Score")
+    plt.ylabel("Moving Average Score")
     plt.show()
     if save == True:
         fig.savefig("/vol/bitbucket/ttc14/thesis/fig/{0}/{1}.png".format(folder,title),dpi=100)
     plt.close()
+
+
+def calcMovingAverage(score,episode_window):
+
+    start = 0
+    end = episode_window-1
+    final = len(score)-1
+    y = []
+    
+    while end <= final:
+        if(start==0):
+            first_mean = sum(score[start:end+1])/episode_window
+            y.append(first_mean)
+        else:
+            mean = y[start-1] - (score[start-1]/episode_window) + (score[end]/episode_window)
+            y.append(mean)
+        start += 1
+        end += 1
+        
+    return y
 
     
 def writeResult(filename,folder,params,run):
@@ -130,48 +185,50 @@ def main():
     '''
     ############ Q-Learning ###############
     
-    num_episode = 180
+    num_episode = 100
     game_step = 100
     gamma  = 0.9
     learning_rate = 0.8
-    discount_noise = True                   # False to not use any Gaussian noise on action selection
+    discount_noise = False                   # False to use epsilon-greedy
     diminishing_weight = True              # False to not use the discounted weight for noise in late episodes
     punish = False                          # True to set a punishment for stepping into the hole in FrozenLake-v0
     punish_val = -1
+    max_epsilon = 1.0                           # maximum epsilon value which decays linearly with episodes
+    min_epsilon = 0.001
 
-    run = 5                                 # Number of runs to train the agent 
+    run = 30                                 # Number of runs to train the agent 
     save = False                            # True to save the picture generated from evalEpisode()
+    episode_window = 50                     # size of the window for moving average
     folder = "frozen_lake_not_slippery"
-    
-    for i in range(run):
         
-        (goals, Q) = qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val,diminishing_weight)
+    for i in range(run):
+        # '''
+        (goals, Q) = qLearning(env,num_episode,gamma,learning_rate,game_step,punish,discount_noise,punish_val,diminishing_weight,max_epsilon,min_epsilon)
         avg_score = sum(goals)/num_episode
         print("Average score per episode:", avg_score)
 
         ############ Using Final Q Table to Play Games without further Update ##################
-        # '''
+
         actual_goals = playGame(Q,env,num_episode,game_step)
         actual_avg = sum(actual_goals)/num_episode
         print("Average actual score per episode:", actual_avg)
         # '''
 
         ############## Store the Result ###############
-        
-        filename = "Tabular_QLearning_Result_of_{0}_{1}_episodes".format(game,num_episode) 
+        # '''
+        filename = "Expo_Epsilon_Decay_Tabular_QLearning_Result_of_{0}_{1}_episodes".format(game,num_episode) 
         params = [num_episode,gamma,learning_rate,discount_noise,punish,i,avg_score,actual_avg]
         
-        # '''
         writeResult(filename,folder,params,i)
         # '''
         
         ############# Plot the Change of Goal against Episode ####################
-        '''    
+        '''
         title = "{0}th_Tabular_QLearning_Result_of_{1}_{2}_episodes".format(i,game,num_episode)
-        evalEpisode(goals,num_episode,title,save,folder)
+        # goals = [1,2,3,4,5,6]
+        evalEpisode(goals,num_episode,episode_window,title,save,folder)
         
         '''
-        
         
 if __name__ == "__main__":
     main()
