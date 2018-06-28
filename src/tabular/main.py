a8803016d55fd599bd213c2d6aab3f55310fa479
Christopher  Chong <tau.chong14@imperial.ac.uk>
2018-06-28 15:49:09 +0100

    fix np.argmax issues and count-based and epsilon is working!

--- src/tabular/main.py
+++ src/tabular/main.py
@@ -98,7 +98,7 @@ def writeResult(filename,folder,params,run):
 
 def main():
 
-    game = "windy_maze" # windy_maze 
+    game = "hard_windy_maze" # windy_maze   # hard_windy_maze
     maze = me.makeMapEnv(game)        # self-created env
     
     ###### Random Action Sampling ########
@@ -119,18 +119,18 @@ def main():
     
     obs_n = maze._obs_space_n                      # self-created env
     act_n = maze._agent._action_space_n   
-    gamma  = 0.8                          # 0.9 for gauss,epsilon
-    learning_rate = 0.5                   # 0.8 for gauss, epsilon
+    gamma  = 0.9                          # 0.9 for gauss,epsilon
+    learning_rate = 0.8                   # 0.8 for gauss, epsilon
     
     max_epsilon = 1.0                      # maximum epsilon value which decays linearly with episodes
     min_epsilon = 0.001
-    discount_noise = False                 # "epsilon" to use epsilon-greedy, False to use count
+    discount_noise = "epsilon"                 # "epsilon" to use epsilon-greedy, False to use greedy
     diminishing_weight = True             # False to not use the discounted weight for noise in late episodes
-    count_const = 0.9                     # count-based exploration constant for exploration bonus
+    beta = 0.9                             # count-based exploration constant for exploration bonus
     
     ####### Experiment Freq ######
     
-    max_episode = 1000
+    max_episode = 2000
     game_step = 100
     run = 1                                 # Number of runs to train the agent 
     save = False                            # True to save the picture generated from evalEpisode()
@@ -142,9 +142,10 @@ def main():
     t_agent = ta.Tabular_Q_Agent(gamma,learning_rate,obs_n,act_n,
                                  max_epsilon,min_epsilon,discount_noise,
                                  diminishing_weight,max_episode)
+
     # '''
     # t_agent.Q[t_agent.Q == 0] = 1/(1-gamma)           # to use OFU principle for initialisation
-    t_agent.Q[t_agent.Q == 0] = 0.4
+    t_agent.Q[t_agent.Q == 0] = 1.0
     
     for i in range(run):
 
@@ -162,13 +163,13 @@ def main():
             while step_count <= game_step:
 
                 action = t_agent.act(state,episode)
-                if(action == np.argmax(t_agent.Q[state,:])):
-                    act_count+=1
+                # if(action == np.argmax(t_agent.Q[state,:])):
+                #     act_count+=1
                     
                 new_state, reward, done = maze.step(action)
                 # t_agent.train(new_state,reward,state,action)       # normal training
-                # t_agent.count_train(new_state,reward,state,action,count_const)         # count-based training
-                t_agent.model_train(new_state,reward,state,action,count_const)         # model-based training
+                t_agent.count_train(new_state,reward,state,action,beta)         # count-based training
+                # t_agent.model_train(new_state,reward,state,action,beta)         # model-based training
                 
                 # print(t_agent.Q)
                 
@@ -188,7 +189,7 @@ def main():
         print("Final Q Table  = \n",t_agent.Q)
         print("Final Count Table  = \n",t_agent.visit_count)
         print("No. of plays under {0} game steps = ".format(game_step),done_count)
-        print("Average greedy action per epi =",sum(action_count)/max_episode)
+        # print("Average greedy action per epi =",sum(action_count)/max_episode)
         avg_score = sum(goals)/max_episode
         print("Average score per episode:", avg_score)
         maze.render()
@@ -199,7 +200,7 @@ def main():
         actual_avg = sum(actual_goals)/max_episode
         print("Average actual score per episode:", actual_avg)
         
-        # '''
+        '''
 
         ############## Store the Result ###############
         # '''
@@ -211,9 +212,9 @@ def main():
         
         ############### Plot the Change of Goal against Episode ####################
         # '''
-        # title = "{0}th_Tabular_QLearning_Result_of_{1}_{2}_episodes".format(i,game,max_episode)
-        # # goals = [1,2,3,4,5,6]
-        # evalEpisode(goals,max_episode,episode_window,title,save,folder)
+        title = "{0}th_Tabular_QLearning_Result_of_{1}_{2}_episodes".format(i,game,max_episode)
+        # goals = [1,2,3,4,5,6]
+        evalEpisode(goals,max_episode,episode_window,title,save,folder)
         
         # '''
 
--- src/tabular/t_agent.py
+++ src/tabular/t_agent.py
@@ -18,7 +18,7 @@ class Tabular_Q_Agent:
         self.diminishing_weight = diminishing_weight
 
         self.visit_count = np.zeros([obs_n,act_n])
-        self.reward = [[[] for action in range(self.act_n)] for state in range(self.obs_n)]
+        #self.reward = [[[] for action in range(self.act_n)] for state in range(self.obs_n)]
 
         
     def act(self,state,episode):
@@ -30,7 +30,7 @@ class Tabular_Q_Agent:
         elif self.discount_noise == "epsilon":
             action = self.epsilonGreedy(state,episode)
         else:
-            action = self.play(state)
+            action = self.optimalAction(state)          # greedy approach
 
         self.visit_count[state,action] += 1
         return action    
@@ -45,23 +45,68 @@ class Tabular_Q_Agent:
         return td_delta
 
 
-    def count_train(self,new_state,reward,state,action,const):
+    ####### with Exploration Bonus ###########
+    
+    def count_train(self,new_state,reward,state,action,beta):
 
-        exploration_bonus  = const / np.sqrt(self.visit_count[state,action])
+        exploration_bonus  = beta/np.sqrt(self.visit_count[state,action])
         optimal_Q = np.max(self.Q[new_state,:])
         td_delta = reward + exploration_bonus + self.gamma*(optimal_Q) - self.Q[state,action]
         self.Q[state,action] += self.learning_rate*(td_delta)
         
         return td_delta
 
-    
-    def model_train(self,new_state,reward,state,action,const):
+        
+    def epsilonGreedy(self,state,episode):
 
-        exploration_bonus  = const / np.sqrt(self.visit_count[state,action])
+        ########## Linear Decay ###############
+        # '''
+        use_epsilon = -(episode/self.max_epi) + self.max_epsilon
+        if(use_epsilon < self.min_epsilon):
+            use_epsilon = self.min_epsilon
+        # ''' 
+        ########## Exponential Decay ##########
+        '''
+        use_epsilon = self.max_epsilon * (1/(episode+1))
+        '''
+        
+        rand_num = np.random.uniform(0,1) #*(1/(episode+1))
+        if(use_epsilon > rand_num):
+            action = np.random.random_integers(0,self.act_n-1)
+        else:
+            action = self.optimalAction(state)
+        
+        return action
+
+
+    def play(self,state):
+
+        action = self.optimalAction(state)
+        return action
+        
+
+    def optimalAction(self,state):
+
+        max_Q = max(self.Q[state,:])
+        index = 0
+        index_list = []
+        
+        for a in self.Q[state,:]:
+            if max_Q == a:
+                index_list.append(index)
+            index += 1
+
+        rand = np.random.choice(index_list)
+        return rand
+
+    ####### Model-Based Training with only mean reward, no mean transition, not functioning ########
+    
+    def model_train(self,new_state,reward,state,action,beta):
+
+        exploration_bonus  = beta / np.sqrt(self.visit_count[state,action])
         mean_reward = self.calcAvgReward(reward,state,action)
         optimal_Q = np.max(self.Q[new_state,:])
         td_delta = mean_reward + exploration_bonus + self.gamma*(optimal_Q) - self.Q[state,action]
-        # td_delta = mean_reward + self.gamma*(optimal_Q) - self.Q[state,action]
         self.Q[state,action] += self.learning_rate*(td_delta)
 
         return td_delta
@@ -73,31 +118,5 @@ class Tabular_Q_Agent:
         mean_r = sum(self.reward[state][action])/self.visit_count[state,action]
         return mean_r
 
-    
-    def epsilonGreedy(self,state,episode):
 
-        ########## Linear Decay ###############
-        '''
-        use_epsilon = -(episode/self.max_epi) + self.max_epsilon
-        if(use_epsilon < self.min_epsilon):
-        use_epsilon = self.min_epsilon
-        ''' 
-        ########## Exponential Decay ##########
-        # '''
-        use_epsilon = self.max_epsilon * (1/(episode+1))
-        # '''
-        
-        rand_num = np.random.uniform(0,1) *(1/(episode+1))
-        if(use_epsilon > rand_num):
-            action = np.random.random_integers(0,self.act_n-1)
-        else:
-            action = np.argmax(self.Q[state,:])
-        
-        return action
-
-    
-    def play(self,state):
-
-        action = np.argmax(self.Q[state,:])
-        return action
-        
+            
